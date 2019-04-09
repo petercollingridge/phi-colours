0736998b2cf250a896d52ca706b9154d22056c62
Peter Collingridge <peter.collingridge@nanoporetech.com>
2019-04-09 13:01:01 +0100

    Show colour range

--- colour-range.js
+++ colour-range.js
@@ -1,3 +1,6 @@
+var PHI = (1 + Math.sqrt(5)) / 2;
+var dHue = PHI * 360;
+
 var vm = new Vue({
     el: '#colour-range',
     data: {
@@ -33,18 +36,63 @@ var vm = new Vue({
 
             this.hue *= 60;
             this.lightness = (cMin + cMax) / 2;
-            this.saturation = d ? d / (1 - Math.abs(2 * this.lightness - 1)) : 0;
+            this.saturation = d ? d / this.absLightness(this.lightness) : 0;
 
             this.color1 = "rgb(" + this.red + ", " + this.green + ", " + this.blue + ")";
 
             return Math.round(this.hue) + ", " + Math.round(this.saturation * 100) / 100 + " " + Math.round(this.lightness * 100) / 100;
         },
-        nColours: function() {
-            return parseInt(this.colourRange);
+        colours: function() {
+            var colours = [];
+            var n = parseInt(this.colourRange);
+
+            for (var i = 0; i < n; i++) {
+                var hue = (this.hue + dHue * i) % 360;
+                var rgb = this.HSL2RGB(hue, this.saturation, this.lightness);
+                colours.push(rgb);
+            }
+            return colours;
         }
     },
     methods: {
+        HSL2RGB: function(hue, saturation, lightness) {
+            var c = this.absLightness(lightness) * saturation;
+            var x = c * (1 - Math.abs((hue / 60) % 2 - 1));
+            var m = lightness - c * 0.5;
 
+            var r = g = b = 0;
+
+            if (hue < 60) {
+                r = c; g = x;
+            } else if (hue < 120) {
+                r = x; g = c;
+            } else if (hue < 180) {
+                g = c; b = x;
+            } else if (hue < 240) {
+                g = x; b = c;
+            } else if (hue < 300) {
+                r = x; b = c;
+            } else {
+                r = c; b = x;
+            } 
+
+            r = (r + m) * 255;
+            g = (g + m) * 255;
+            b = (b + m) * 255;
+
+            return [r, g, b];
+        },
+        absLightness: function(lightness) {
+            return 1 - Math.abs(2 * lightness - 1);
+        },
+        getColourString: function(rgb) {
+            return "rgb(" + rgb.map(Math.round).join(", ") + ")";
+        },
+        getStyle: function(rgb) {
+            return {
+                backgroundColor: this.getColourString(rgb)
+            };
+        }
     },
 
 });
